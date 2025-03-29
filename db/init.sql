38ac055b23747bbe6f87ee07047349c3e38cc554
CarEsteban <car23016@uvg.edu.gt>
2025-03-28 18:08:48 -0600

    initial creation for BD

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,4 @@
+-- Crear la tabla
 CREATE TABLE IF NOT EXISTS matches (
     id SERIAL PRIMARY KEY,
     home_team VARCHAR(100) NOT NULL,
@@ -11,3 +12,18 @@ CREATE TABLE IF NOT EXISTS matches (
     away_red_cards INT DEFAULT 0,
     extra_time BOOLEAN DEFAULT FALSE
 );
+
+-- Insertar datos iniciales
+INSERT INTO matches (home_team, away_team, match_date)
+VALUES
+  ('Barcelona', 'Real Madrid', '2026-04-01'),
+  ('Atletico Madrid', 'Sevilla', '2025-04-02'),
+  ('Valencia', 'Villarreal', '2025-04-03'),
+  ('Real Sociedad', 'Athletic Club', '2025-04-04'),
+  ('Betis', 'Getafe', '2025-04-05'),
+  ('Espanyol', 'Celta de Vigo', '2025-04-06'),
+  ('Levante', 'Real Valladolid', '2025-04-07'),
+  ('Granada', 'Mallorca', '2025-04-08'),
+  ('Cadiz', 'Elche', '2025-04-09'),
+  ('Almeria', 'Osasuna', '2025-04-10');
+
